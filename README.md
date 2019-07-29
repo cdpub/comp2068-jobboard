a0cb57d2b8c6c8df78e1c693cdb94135f55e3d7d
CynthiaPublico <Cynthia.Dejesus-Publico@MyGeorgian.ca>
2019-07-28 20:38:57 -0400

    Created Job Board REACT version with 3 components.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# comp2068-jobboard
 Job Board is REACT application with 3 components created under COMP2068 Lab3
---
+++ app.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI, {
+    auth:   {
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD
+    },
+    useNewUrlParser: true            
+}).catch(err => console.log(`ERROR: ${err}`));
+
+const express = require(`express`);                      
+const app = express();                              
+
+//add cookies and session support to app
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+app.use(cookieParser());
+app.use(session({
+  secret: (process.env.secret || 'yeah'),
+  cookie: {
+    max: 10800000
+  },
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flash = res.locals.flash || {};
+  res.locals.flash.success = req.flash('success') || null;
+  res.locals.flash.error = req.flash('error') || null;
+
+  next();
+});
+
+
+//import and register body parser library
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());                 
+app.use(bodyParser.urlencoded({             
+    extended: true
+}));
+
+
+// Create path and configure views
+const path = require('path');          
+app.set(`views`, path.join(__dirname, `views`));            
+app.set(`view engine`, `pug`);
+
+app.use('/css', express.static('assets/stylesheets'));      
+app.use('/js', express.static('assets/javascripts'));
+app.use('/images', express.static('assets/images'));
+
+//auth helper utility , returns users id
+const isAuthenticated = (req) => {
+  return req.session && req.session.userId;
+};
+
+app.use((req, res, next) => {
+  req.isAuthenticated = () => {
+    if(!isAuthenticated(req))  {
+      req.flash('error', `You are not permittted to do this action`);
+      res.redirect('/');
+    }
+  }
+  res.locals.isAuthenticated = isAuthenticated(req);
+  next();
+});
+
+//create and register routes
+const routes = require(`./routes.js`);
+app.use(`/`, routes);                                       
+
+
+// create dynamic port
+const port = process.env.PORT || 4000;       
+app.listen(port, () => console.log(`Listening on port ${port}`));    
